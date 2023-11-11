6679949beacf468d107471104c87f3cd8718e266
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2023-11-11 19:38:44 -0300

    flow control

--- recursion/base_case.py
+++ recursion/base_case.py
@@ -2,6 +2,11 @@ def mult(a, b):
     if b == 1:
         return a
     else:
-        return a + mult(a, b-1)
+        return a + mult(a, b-1) ## creamos un scope y un flow de control
     
 print(mult(10, 5)) ## 50
+
+## si fuera factorial
+## fact(n) return n*fact(n-1)
+
+
