9f4a0185c49f2c1740a13e8476f73610c6cf3d2c
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2023-11-11 19:41:23 -0300

    Update base_case.py

--- recursion/base_case.py
+++ recursion/base_case.py
@@ -2,11 +2,12 @@ def mult(a, b):
     if b == 1:
         return a
     else:
-        return a + mult(a, b-1) ## creamos un scope y un flow de control
+        return a + mult(a, b-1) ## creamos un scope tenemos control
     
 print(mult(10, 5)) ## 50
 
 ## si fuera factorial
 ## fact(n) return n*fact(n-1)
+## iteraciones: facto_iter(n) return ()
 
 
